478e7b3fb961251ddd4575e6d0641712a28d86ba
KlangChong <kleong153@gmail.com>
2020-03-19 13:48:16 +0800

    Prepare base files.

---
+++ src/main/java/com/alcodes/alcodessmgalleryviewer/activities/MainActivity.java
@@ -0,0 +1,37 @@
+package com.alcodes.alcodessmgalleryviewer.activities;
+
+import android.os.Bundle;
+
+import com.alcodes.alcodessmgalleryviewer.R;
+import com.alcodes.alcodessmgalleryviewer.databinding.AsmGvrActivityMainBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AsmGvrActivityMainBinding mDataBinding;
+    private NavController mNavController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Init data binding.
+        mDataBinding = AsmGvrActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(mDataBinding.getRoot());
+
+        // Init navigation components.
+        mNavController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        NavigationUI.setupActionBarWithNavController(this, mNavController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return mNavController.navigateUp() || super.onSupportNavigateUp();
+    }
+}
---
+++ src/main/java/com/alcodes/alcodessmgalleryviewer/fragments/MainFragment.java
@@ -0,0 +1,31 @@
+package com.alcodes.alcodessmgalleryviewer.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alcodes.alcodessmgalleryviewer.databinding.AsmGvrFragmentMainBinding;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    private AsmGvrFragmentMainBinding mDataBinding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Init data binding;
+        mDataBinding = AsmGvrFragmentMainBinding.inflate(inflater, container, false);
+
+        return mDataBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
