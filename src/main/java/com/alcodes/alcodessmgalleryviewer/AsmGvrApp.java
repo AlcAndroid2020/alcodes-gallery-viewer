3079703ed7b3b1a3001f88780be025862e126d01
KlangChong <kleong153@gmail.com>
2020-03-19 15:24:07 +0800

    Add in reference resources.

--- src/main/java/com/alcodes/alcodessmgalleryviewer/AsmGvrApp.java
+++ src/main/java/com/alcodes/alcodessmgalleryviewer/AsmGvrApp.java
@@ -6,6 +6,11 @@ import timber.log.Timber;
 
 public class AsmGvrApp {
 
+    // TODO REF: https://medium.com/@deepakpk/how-to-add-a-git-android-library-project-as-a-sub-module-c713a653ab1f
+    // TODO REF: https://developer.android.com/studio/projects/add-app-module
+    // TODO REF: https://proandroiddev.com/your-android-libraries-should-not-ask-an-application-context-51986cc140d4
+    // TODO REF: https://archie94.github.io/blogs/working-with-submodules-in-git-and-android-studio
+
     public static final String TAG = AsmGvrApp.class.getSimpleName();
 
     public static void init(Application application) {
