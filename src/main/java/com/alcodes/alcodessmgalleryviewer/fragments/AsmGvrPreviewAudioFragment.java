fea63e5bd5483011959dd4017f14ce09e65c90ae
cstanALC <cstan.alc@gmail.com>
2020-04-17 17:39:50 +0800

    -Added show details option menu with multiple language for audio,fix some bug and format code

--- src/main/java/com/alcodes/alcodessmgalleryviewer/fragments/AsmGvrPreviewAudioFragment.java
+++ src/main/java/com/alcodes/alcodessmgalleryviewer/fragments/AsmGvrPreviewAudioFragment.java
@@ -2,6 +2,7 @@ package com.alcodes.alcodessmgalleryviewer.fragments;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -15,13 +16,14 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.MediaController;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.documentfile.provider.DocumentFile;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -42,6 +44,9 @@ import com.vincan.medialoader.DownloadManager;
 import com.vincan.medialoader.MediaLoader;
 
 import java.io.File;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AsmGvrPreviewAudioFragment extends Fragment implements CacheListener {
 
@@ -89,33 +94,95 @@ public class AsmGvrPreviewAudioFragment extends Fragment implements CacheListene
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.asm_gvr_audio_menu, menu);
-        if(!mInternetSource)
+        if (!mInternetSource) {
+            //for local file hide download option
             menu.findItem(R.id.audio_menu_download).setVisible(false);
+        } else {
+            //for online hide file details
+            menu.findItem(R.id.audio_menu_details).setVisible(false);
+
+        }
+
 
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
 
-        if(itemId == R.id.audio_menu_open_with) {
+        if (itemId == R.id.audio_menu_open_with) {
             //open audio with other app
-          mGvrOpenWithConfig.openWith(getContext(),Uri.parse(mViewPagerURL));
+            mGvrOpenWithConfig.openWith(getContext(), Uri.parse(mViewPagerURL));
 
-        }else if(itemId == R.id.audio_menu_download) {
+        } else if (itemId == R.id.audio_menu_download) {
             //open file picker to select folder to download
             Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
             intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
             startActivityForResult(intent, 42);
-        }
-        else if(itemId==R.id.audio_menu_share){
+        } else if (itemId == R.id.audio_menu_share) {
             //share audio
-          mGvrShareConfig.shareWith(getContext(),Uri.parse(mViewPagerURL));
-
+            mGvrShareConfig.shareWith(getContext(), Uri.parse(mViewPagerURL));
 
         }
-
+        else if (itemId == R.id.audio_menu_details) {
+            //show audio file details
+            showdetails();
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void showdetails() {
+        // show audio file details
+        Uri uri = Uri.parse(mViewPagerURL);
+        DocumentFile df = DocumentFile.fromSingleUri(getContext(), uri);
+        String line1 = getResources().getString(R.string.filename) + ": " + df.getName() + "\n";
+        String line2 = getResources().getString(R.string.filetype) + ": " + df.getType() + "\n";
+        String size = "";
+        //format size
+        if (df.length() > 0) {
+
+            //convert bytes to mb size
+            DecimalFormat format = new DecimalFormat("#.##");
+            long MiB = 1024 * 1024;
+            long KiB = 1024;
+
+            double length = Double.parseDouble(String.valueOf(df.length()));
+
+            if (length > KiB) {
+                size = format.format(length / KiB) + " KB";
+            }
+            if (length > MiB) {
+                size = format.format(length / MiB) + " MB";
+            } else
+                size = format.format(length) + " B";
+
+        } else
+            size = "0 MB";
+
+        String line3 =  getResources().getString(R.string.file_size)+": "+ size + "\n";
+
+        //format date
+        Date d = new Date(df.lastModified());
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        String newDate = formatter.format(d);
+        String line4 = getResources().getString(R.string.lastmodif) + ": " + newDate + "\n";
+        String line5 = getResources().getString(R.string.path) + ": " + uri.getPath() + "\n";
+
+
+        AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+
+
+                .setNegativeButton( getResources().getString(R.string.Okay), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setMessage(line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n" + line5).show();
+
+
     }
 
 
@@ -131,14 +198,14 @@ public class AsmGvrPreviewAudioFragment extends Fragment implements CacheListene
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        if(mGvrDownloadConfig == null)
-           mGvrDownloadConfig = new AsmGvrDownloadConfig();
+        if (mGvrDownloadConfig == null)
+            mGvrDownloadConfig = new AsmGvrDownloadConfig();
 
-        if(mGvrShareConfig == null)
-           mGvrShareConfig = new AsmGvrShareConfig();
+        if (mGvrShareConfig == null)
+            mGvrShareConfig = new AsmGvrShareConfig();
 
-        if(mGvrOpenWithConfig==null)
-            mGvrOpenWithConfig=new AsmGvrOpenWithConfig();
+        if (mGvrOpenWithConfig == null)
+            mGvrOpenWithConfig = new AsmGvrOpenWithConfig();
 
         // Extract arguments.
         mViewPagerPosition = requireArguments().getInt(ARG_INT_PAGER_POSITION);
@@ -161,13 +228,14 @@ public class AsmGvrPreviewAudioFragment extends Fragment implements CacheListene
         mMainSharedViewModel.getColorSelectedLiveData().observe(getViewLifecycleOwner(), new Observer<Integer>() {
             @Override
             public void onChanged(Integer integer) {
-                if(integer!=null){
-                    mDataBinding.previewAudioRoot.setBackgroundColor(ContextCompat.getColor(getActivity(),  integer));
+                if (integer != null) {
+                    mDataBinding.previewAudioRoot.setBackgroundColor(ContextCompat.getColor(getActivity(), integer));
 
                 }
             }
         });
 
+        //resume if screen rotation
         mMainSharedViewModel.getViewPagerPositionLiveData().observe(getViewLifecycleOwner(), new Observer<Integer>() {
 
             @Override
@@ -333,9 +401,7 @@ public class AsmGvrPreviewAudioFragment extends Fragment implements CacheListene
     @Override
     public void onPause() {
         super.onPause();
-        //   if (mDataBinding.AudioPlayer.isPlaying()) {
         mPreviewAudioViewModel.setViewPagerVideoViewLiveData(mViewPagerPosition, mDataBinding.AudioPlayer.getCurrentPosition());
-        // }
 
     }
 
@@ -344,6 +410,7 @@ public class AsmGvrPreviewAudioFragment extends Fragment implements CacheListene
     public void onCacheAvailable(File cacheFile, String url, int percentsAvailable) {
 
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -351,10 +418,9 @@ public class AsmGvrPreviewAudioFragment extends Fragment implements CacheListene
             //Start download using Download utils class
             if (requestCode == 42) {
                 if (data != null) {
-                   mGvrDownloadConfig.startDownload(requireActivity(),mViewPagerURL,data.getData());
+                    mGvrDownloadConfig.startDownload(requireActivity(), mViewPagerURL, data.getData());
                 }
             }
-            //Start download using Download utils class
         }
     }
 }
--- src/main/java/com/alcodes/alcodessmgalleryviewer/utils/AsmGvrShareConfig.java
+++ src/main/java/com/alcodes/alcodessmgalleryviewer/utils/AsmGvrShareConfig.java
@@ -1,9 +1,19 @@
 package com.alcodes.alcodessmgalleryviewer.utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.StrictMode;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.documentfile.provider.DocumentFile;
+
+import java.io.File;
 
 public class AsmGvrShareConfig {
 
@@ -32,4 +42,7 @@ public class AsmGvrShareConfig {
             context.startActivity(Intent.createChooser(shareIntent, "Share With..."));
         }
     }
+
+
+
 }
